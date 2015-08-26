c5d26fbc03969fc8ed6fb63712ae022964c99043
Jonathan BOULOUX <jbouloux@e2r8p20.42.fr>
2015-08-26 19:35:25 +0200

    Palier 00 termine

---
+++ Palier_00/aff_first_param/aff_first_param.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   aff_first_param.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbouloux <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/08/26 18:32:47 by jbouloux          #+#    #+#             */
+/*   Updated: 2015/08/26 18:48:02 by jbouloux         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int		main(int argc, char **argv)
+{
+	if (argc > 2)
+		ft_putstr(argv[1]);
+	ft_putchar('\n');
+	return (0);
+}
---
+++ Palier_00/aff_last_param/aff_last_param.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   aff_last_param.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbouloux <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/08/26 18:32:47 by jbouloux          #+#    #+#             */
+/*   Updated: 2015/08/26 19:03:19 by jbouloux         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int		main(int argc, char **argv)
+{
+	if (argc > 2)
+		ft_putstr(argv[argc - 1]);
+	ft_putchar('\n');
+	return (0);
+}
